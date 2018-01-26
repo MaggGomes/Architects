579ff1758814f0a80f58b822517f9e0353963aa7
MaggGomes <magg.slb@gmail.com>
2018-01-26 22:58:39 +0000

    team page done

--- public/javascripts/rochaleiteApp.js
+++ public/javascripts/rochaleiteApp.js
@@ -102,8 +102,20 @@ app.config(function($routeProvider,$locationProvider) {
             templateUrl : "views/partials/project.htm",
             controller : "projectCtrl"
         })
+        .when("/arquitectos", {
+            templateUrl : "views/partials/arquitectos.htm"
+        })
         .when("/team", {
             templateUrl : "views/partials/team.htm"
+        })
+        .when("/curriculum", {
+            templateUrl : "views/partials/curriculum.htm"
+        })
+        .when("/distinctions", {
+            templateUrl : "views/partials/distinctions.htm"
+        })
+        .when("/links", {
+            templateUrl : "views/partials/links.htm"
         })
         .when("/contacts", {
             templateUrl : "views/partials/contacts.htm",
@@ -225,3 +237,24 @@ app.controller('contactsCtrl', function($scope, $http, $translate) {
 
 });
 // Views controllers (end)
+
+app.animation('.slide', [function() {
+    return {
+        // make note that other events (like addClass/removeClass)
+        // have different function input parameters
+        enter: function(element, doneFn) {
+            jQuery(element).fadeIn(1000, doneFn);
+
+            // remember to call doneFn so that angular
+            // knows that the animation has concluded
+        },
+
+        move: function(element, doneFn) {
+            jQuery(element).fadeIn(1000, doneFn);
+        },
+
+        leave: function(element, doneFn) {
+            jQuery(element).fadeOut(1000, doneFn);
+        }
+    }
+}]);

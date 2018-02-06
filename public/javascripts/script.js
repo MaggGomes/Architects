61363d4920bdef12baa818912472b84eecc3b9b9
MaggGomes <magg.slb@gmail.com>
2018-02-06 19:31:09 +0000

    text and margins in contacts page fixed

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,5 @@
 $(document).ready(function () {
 
-    // TODO - verificar se é para fazer desaprecer o div com click ou após alguns segundos
-    /*$('#main-screen-display').click(function(){
-        $(this).fadeOut(2000, function(){
-            $('.hidden-container').fadeIn(2000);
-        });
-    });*/
-
 
 
 });
